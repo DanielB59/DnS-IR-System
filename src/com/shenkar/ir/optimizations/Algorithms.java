f3444ca4d1933daed47d0443d11775bc4b4bc078
Daniel Ben Zvi <danielbenzvi0177@gmail.com>
2018-03-17 21:59:56 +0200

    soundex & stemming
    algorithm implementations

--- src/com/shenkar/ir/optimizations/Algorithms.java
+++ src/com/shenkar/ir/optimizations/Algorithms.java
@@ -1,6 +1,59 @@
 package com.shenkar.ir.optimizations;
 
+import java.util.HashMap;
+
 public class Algorithms {
 	private Algorithms() {};
 	//probably visitor design pattern
+	
+	public static final HashMap<String, String> dictionary = new HashMap<String, String>(){{
+		//put("AEHIOUWY","0");
+		put("BFPV","1");
+		put("CGJKQSXZ","2");
+		put("DT","3");
+		put("L","4");
+		put("MN","5");
+		put("R","6");
+	}};
+	
+	public static String soundex(String term) {
+		StringBuilder builder = new StringBuilder("");
+		String result = null, code = null;
+		
+		term = term.toUpperCase();
+		builder.append(term.charAt(0));
+		
+		char[] letters = term.toCharArray();
+		String prev = "";
+		
+		for (int i = 1; i < letters.length; ++i) {
+			for (String key : dictionary.keySet()) {
+				if (0 <= key.indexOf(letters[i])) {
+					code = dictionary.get(key);
+					if (null != code && prev != code) {
+						prev = code;
+						builder.append(code);
+					}
+				}
+			}
+		}
+		
+		//result = builder.toString().replace("0", "");
+		builder.append("000");
+		result = builder.substring(0,4);
+		
+		return result;
+	}
+	
+	private static Stemmer stemmer = new Stemmer();
+	
+	public static String stem(String term) {
+		char[] arr = term.toCharArray();
+		
+		stemmer.reset();
+		stemmer.add(arr, arr.length);
+		stemmer.stem();
+		
+		return stemmer.toString();
+	}
 }

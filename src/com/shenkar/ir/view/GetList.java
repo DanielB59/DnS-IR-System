9e2ad609f055d7be81c19574e4519b9af5d53522
sagi741 <sagi741@gmail.com>
2018-03-22 02:31:33 +0200

    button
    button

--- src/com/shenkar/ir/view/GetList.java
+++ src/com/shenkar/ir/view/GetList.java
@@ -1,16 +1,32 @@
 package com.shenkar.ir.view;
 
+import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import com.shenkar.ir.entities.Document;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.shape.Path;
+
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.JList;
 import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.awt.Panel;
 
 public class GetList {
 
@@ -50,19 +66,39 @@ public class GetList {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 261);
-		frame.getContentPane().add(panel);
+		JPanel panel1 = new JPanel();
+		panel1.setBounds(0, 0, 295, 261);
+		frame.getContentPane().add(panel1);
+		panel1.setLayout(null);
 		
 		ArrayList<Document> list = new ArrayList<Document>();
         Document d = new Document();
         list.add(d);
 		
 		TextArea textArea = new TextArea();
-		textArea.setText(d.toString());
-		panel.add(textArea);
+		textArea.setBounds(10, 10, 275, 241);
+		for (int i = 0 ; i < list.size() ; i++){
+        	textArea.setText(d.toString());
+        	textArea.setText("\n");  
+        }	
+		panel1.add(textArea);
+		
+		Panel panel2 = new Panel();
+		panel2.setBounds(301, 0, 133, 261);
+		frame.getContentPane().add(panel2);
+		
+		for (int i = 0 ; i < list.size() ; i++){
+        	JButton b = new JButton();  
+        	panel2.add(b);
+        }
 		
 		
+		
+		
+		
+        	
 		
 	}
 }
+
+
